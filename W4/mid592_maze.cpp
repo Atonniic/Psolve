c4b06af41f38211791eb4b5956e4b98e8651f913
mkksdx2 <chaipot.k@ku.th>
2023-02-20 13:47:33 +0700

    19/02/2023

--- W4/mid592_maze.cpp
+++ W4/mid592_maze.cpp
@@ -1,18 +1,66 @@
 #include <iostream>
+#include <queue>
 
-char a[30][30];
+char mat[30][30];
+int visited[30][30], di[] = {-1,0,1,0}, dj[] = {0,1,0,-1};
+struct A{
+    int i, j, key;
+};
+std::queue<A> bfs;
 
-int main() {
-    int n, q;
+int main(){
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(NULL);
+    int n, q, r, c, a, b, count = 0;
 
     std::cin >> n >> q;
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            std::cin >> a[i][j];
-        }
+    for (int i = 1; i <= n; i++){
+        for (int j = 1; j <= n; j++){
+            std::cin >> mat[i][j];
+        }   
     }
-    for (int i = 0; i < q; i++) {
-        int r, c, a, b;
+    while(q--){
         std::cin >> r >> c >> a >> b;
         
+        count++;
+        bfs.push({r, c, 1});
+        while(!bfs.empty()){
+            auto tmp = bfs.front();
+            bfs.pop();
+            if (visited[tmp.i][tmp.j] == count) {
+                continue;
+            } 
+            visited[tmp.i][tmp.j] = count;
+            if (tmp.i == a && tmp.j == b) {
+                std::cout << "yes\n";
+                goto x;
+            }
+            for (int i = 0; i < 4; i++) {
+                int tmp_i = tmp.i + di[i];
+                int tmp_j = tmp.j + dj[i];
+
+                if (tmp_i <= 0 || tmp_j <= 0) {
+                    continue;
+                }
+                if (tmp_i > n || tmp_j > n) {
+                    continue;
+                }
+                if (mat[tmp_i][tmp_j] == '#') {
+                    continue;
+                }
+                if (mat[tmp_i][tmp_j] == '.') {
+                    bfs.push({tmp_i, tmp_j, tmp.key});
+                }
+                if (mat[tmp_i][tmp_j] == 'O' && tmp.key == 1) {
+                    bfs.push({tmp_i, tmp_j, 0});
+                }
+            }
+        }
+        std::cout << "no\n";
+        x:;
+        while(!bfs.empty()) {
+            bfs.pop();
+        }
+    }
+    return 0;
 }
--- W4/mid64_shipmentcost.cpp
+++ W4/mid64_shipmentcost.cpp
@@ -2,35 +2,38 @@
 #include <vector>
 #include <queue>
 
-std::vector<int> g[100100], fact;
+std::vector<int> v[100100], fact;
 std::queue<int> st, fc;
-int mark[100100], storage[100100], factory[100100], ans[100100];
+int visited[100100], storave[100100], factory[100100], ans[100100];
 struct A{
-    int v,d;
+    int v, d;
 };
 std::queue<A> bfs;
 
 int main() {
-    int n, m, s, t, u,tmp, v, i, dis, k;
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(NULL);
+    int n, m, s, t, a, b, tmp, dis, k;
+
     std::cin >> n >> m >> s >> t;
-    for (i = 0; i < m; i++) {
-        std::cin >> u >> v;
-        g[u].push_back(v);
-        g[v].push_back(u);
+    for (int i = 0; i < m; i++) {
+        std::cin >> a >> b;
+        v[a].push_back(b);
+        v[b].push_back(a);
     }
-    for (i = 0; i < s; i++) {
+    for (int i = 0; i < s; i++) {
         std::cin >> tmp;
-        storage[tmp] = 1;
+        storave[tmp] = 1;
         st.push(tmp);
     }
-    for (i = 0; i < t; i++) {
+    for (int i = 0; i < t; i++) {
         std::cin >> tmp;
         factory[tmp] = 1;
         fc.push(tmp);
         fact.push_back(tmp);
     }
     if (s <= t) {
-        for(i = 0; i < s; i++) {
+        for(int i = 0; i < s; i++) {
             tmp = st.front();
             st.pop();
             bfs.push({tmp, 0});
@@ -39,54 +42,56 @@ int main() {
             tmp = bfs.front().v;
             dis = bfs.front().d;
             bfs.pop();
-            if(mark[tmp]) {
+            if (visited[tmp]) {
                 continue;
             }
-            mark[tmp] = 1;
-            if(factory[tmp]) {
-                for(i=0;i<fact.size();i++) {
-                    if(fact[i]==tmp){
+            visited[tmp] = 1;
+            if (factory[tmp]) {
+                for (int i = 0; i < fact.size(); i++) {
+                    if (fact[i]==tmp){
                         ans[i] = dis;
                         break;
                     } 
                 }
             } 
-            for(auto x:g[tmp]) {
-                if(mark[x]) {
+            for (auto x: v[tmp]) {
+                if (visited[x]) {
                     continue;
                 }
-                bfs.push({x,dis+1});
+                bfs.push({x, dis+1});
             }
         }
     }
     else {
-        for(k = 0; k < t; k++) {
+        for (k = 0; k < t; k++) {
             tmp = fc.front();
             fc.pop();
             bfs.push({tmp, 0});
-            while(!bfs.empty()) {
+            while (!bfs.empty()) {
                 tmp = bfs.front().v;
                 dis = bfs.front().d;
                 bfs.pop();
-                if(mark[tmp]) {
+                if (visited[tmp]) {
                     continue;
                 }
-                mark[tmp] = 1;
-                if(storage[tmp]) {
+                visited[tmp] = 1;
+                if (storave[tmp]) {
                     ans[k] = dis;
                     break;
                 }
-                for(auto x:g[tmp]) {
-                    if(mark[x]) continue;
+                for (auto x: v[tmp]) {
+                    if (visited[x]) {
+                        continue;
+                    }
                     bfs.push({x,dis+1});
                 }
             }
-            while(!bfs.empty()) {
+            while (!bfs.empty()) {
                 bfs.pop();
             }
         }
     }
-    for(i = 0; i < t; i++) {
+    for (int i = 0; i < t; i++) {
         std::cout << ans[i] << "\n";
     }
     return 0;
